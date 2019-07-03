5ae380a14667cebf3d3ad747ec0c5f2adf659d11
valenbia36 <vbianchi16@gmail.com>
2019-07-03 15:20:20 -0300

    indx

--- Pagina Final VALEN PASCHE/idx.js
+++ Pagina Final VALEN PASCHE/idx.js
@@ -13,3 +13,13 @@ function login(showhide){
 		'height='+h+',width='+w+',top='+TopPosition+',left='+LeftPosition+',scrollbars='+scroll+',resizable'
 		popupWindow = window.open(url,winName,settings)
 	}
+
+
+var slider = document.getElementById("range1");
+var output = document.getElementById("demo1");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}

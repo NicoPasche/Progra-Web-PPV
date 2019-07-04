9a15e6765eb9a05e64bf8c270f06282a5567baed
Pablodubosc <pablodubosc@hotmail.com>
2019-07-03 21:32:43 -0300

    Update nv_prop.sql

--- Pagina Final VALEN PASCHE/nv_prop.sql
+++ Pagina Final VALEN PASCHE/nv_prop.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 4.6.5.2
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 03-07-2019 a las 23:59:55
--- Versión del servidor: 10.1.36-MariaDB
--- Versión de PHP: 7.2.11
+-- Tiempo de generación: 04-07-2019 a las 02:29:51
+-- Versión del servidor: 10.1.21-MariaDB
+-- Versión de PHP: 5.6.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -25,34 +23,68 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `barrios`
+--
+
+CREATE TABLE `barrios` (
+  `id` int(255) NOT NULL,
+  `nombre` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `compras`
+--
+
+CREATE TABLE `compras` (
+  `idpublicacion` int(255) NOT NULL,
+  `idusuario` int(255) NOT NULL,
+  `fechaseleccionada` date NOT NULL,
+  `id` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `guardadasporusuario`
+--
+
+CREATE TABLE `guardadasporusuario` (
+  `idpublicacion` int(11) NOT NULL,
+  `idusuario` int(11) NOT NULL,
+  `id` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `publicaciones`
 --
 
 CREATE TABLE `publicaciones` (
-  `ID_publicacion` int(255) NOT NULL,
-  `ID_Tipo` int(255) NOT NULL,
-  `Domicilio` varchar(255) NOT NULL,
-  `idBarrio` int(255) NOT NULL,
-  `Numero de Habitaciones` int(255) NOT NULL,
-  `Precio` int(255) NOT NULL,
-  `Numero de Contacto` varchar(255) NOT NULL,
-  `Email` varchar(255) NOT NULL,
-  `Descripcion` varchar(255) NOT NULL,
-  `Fecha de Publicacion` date NOT NULL,
-  `iDImg` int(255) NOT NULL,
-  `ID_usuario` int(255) NOT NULL,
-  `Propiedad Para` varchar(255) NOT NULL
+  `id` int(255) NOT NULL,
+  `idtipo` int(255) NOT NULL,
+  `idbarrio` int(255) NOT NULL,
+  `domicilio` varchar(255) NOT NULL,
+  `numerodehabitaciones` int(255) NOT NULL,
+  `precio` int(255) NOT NULL,
+  `descripcion` varchar(255) NOT NULL,
+  `fechadepublicacion` date NOT NULL,
+  `imagenes` varchar(255) NOT NULL,
+  `idusuario` int(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
+
 --
--- Volcado de datos para la tabla `publicaciones`
+-- Estructura de tabla para la tabla `tipo`
 --
 
-INSERT INTO `publicaciones` (`ID_publicacion`, `ID_Tipo`, `Domicilio`, `idBarrio`, `Numero de Habitaciones`, `Precio`, `Numero de Contacto`, `Email`, `Descripcion`, `Fecha de Publicacion`, `iDImg`, `ID_usuario`, `Propiedad Para`) VALUES
-(1, 0, 'Juana Manso 305, Piso 3A', 1, 3, 30000, '1145402569', 'juanp@gmail.com', '', '2018-11-13', 1, 1, 'Alquilar'),
-(2, 0, 'Nueva York 3512', 1, 2, 3000000, '1145876399', 'paschen@gmail.com', '', '2018-11-04', 2, 2, 'Comprar'),
-(3, 0, '', 1, 0, 10000000, '1145402569', 'juanp@gmail.com', '', '2018-11-06', 3, 1, 'Comprar'),
-(4, 0, 'Montevideo 58', 1, 2, 25000, '1145876399', 'paschen@gmail.com', '', '2018-11-09', 4, 2, 'Alquilar');
+CREATE TABLE `tipo` (
+  `id` int(255) NOT NULL,
+  `tipo` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -61,72 +93,123 @@ INSERT INTO `publicaciones` (`ID_publicacion`, `ID_Tipo`, `Domicilio`, `idBarrio
 --
 
 CREATE TABLE `usuarios` (
-  `ID_usuario` int(255) NOT NULL,
-  `Usuario` varchar(255) NOT NULL,
-  `Contrasenia` varchar(255) NOT NULL,
-  `Nombre` varchar(255) NOT NULL,
-  `Apellido` varchar(255) NOT NULL,
-  `Domicilio` varchar(255) NOT NULL,
-  `Nro Telefono` varchar(255) NOT NULL,
-  `Email` varchar(255) NOT NULL,
-  `Mas Info` varchar(255) NOT NULL
+  `id` int(255) NOT NULL,
+  `usuario` varchar(255) NOT NULL,
+  `contrasenia` varchar(255) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `apellido` varchar(255) NOT NULL,
+  `domicilio` varchar(255) NOT NULL,
+  `nrotelefono` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `info` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Volcado de datos para la tabla `usuarios`
---
-
-INSERT INTO `usuarios` (`ID_usuario`, `Usuario`, `Contrasenia`, `Nombre`, `Apellido`, `Domicilio`, `Nro Telefono`, `Email`, `Mas Info`) VALUES
-(1, 'Juan123', 'muestra123', 'Juan', 'Perez', 'Pueyrredon 2000, CABA', '1145402569', 'juanp@gmail.com', ''),
-(2, 'Paschen', 'muestra2123', 'Nicolas', 'Paschetta', 'Peron 100, CABA', '1145876399', 'paschen@gmail.com', '');
 
 --
 -- Índices para tablas volcadas
 --
 
 --
+-- Indices de la tabla `barrios`
+--
+ALTER TABLE `barrios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `compras`
+--
+ALTER TABLE `compras`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idusuario` (`idusuario`),
+  ADD KEY `idpublicacion` (`idpublicacion`);
+
+--
+-- Indices de la tabla `guardadasporusuario`
+--
+ALTER TABLE `guardadasporusuario`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idusuario` (`idusuario`),
+  ADD KEY `idpublicacion` (`idpublicacion`);
+
+--
 -- Indices de la tabla `publicaciones`
 --
 ALTER TABLE `publicaciones`
-  ADD PRIMARY KEY (`ID_publicacion`),
-  ADD KEY `ID_usuario` (`ID_usuario`),
-  ADD KEY `idBarrio` (`idBarrio`),
-  ADD KEY `iDImg` (`iDImg`),
-  ADD KEY `Tipo` (`ID_Tipo`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idtipo` (`idtipo`),
+  ADD KEY `idbarrio` (`idbarrio`),
+  ADD KEY `idusuario` (`idusuario`);
+
+--
+-- Indices de la tabla `tipo`
+--
+ALTER TABLE `tipo`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`ID_usuario`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
+-- AUTO_INCREMENT de la tabla `barrios`
+--
+ALTER TABLE `barrios`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `compras`
+--
+ALTER TABLE `compras`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `guardadasporusuario`
+--
+ALTER TABLE `guardadasporusuario`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT de la tabla `publicaciones`
 --
 ALTER TABLE `publicaciones`
-  MODIFY `ID_publicacion` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `tipo`
+--
+ALTER TABLE `tipo`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `ID_usuario` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
 --
 -- Restricciones para tablas volcadas
 --
 
 --
+-- Filtros para la tabla `compras`
+--
+ALTER TABLE `compras`
+  ADD CONSTRAINT `compras_ibfk_1` FOREIGN KEY (`idpublicacion`) REFERENCES `publicaciones` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `compras_ibfk_2` FOREIGN KEY (`idusuario`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `guardadasporusuario`
+--
+ALTER TABLE `guardadasporusuario`
+  ADD CONSTRAINT `guardadasporusuario_ibfk_1` FOREIGN KEY (`idpublicacion`) REFERENCES `publicaciones` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `guardadasporusuario_ibfk_2` FOREIGN KEY (`idusuario`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
 -- Filtros para la tabla `publicaciones`
 --
 ALTER TABLE `publicaciones`
-  ADD CONSTRAINT `publicaciones_ibfk_1` FOREIGN KEY (`ID_usuario`) REFERENCES `usuarios` (`ID_usuario`),
-  ADD CONSTRAINT `publicaciones_ibfk_2` FOREIGN KEY (`idBarrio`) REFERENCES `barrio` (`idBarrio`);
-COMMIT;
+  ADD CONSTRAINT `publicaciones_ibfk_1` FOREIGN KEY (`idusuario`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `publicaciones_ibfk_2` FOREIGN KEY (`idtipo`) REFERENCES `tipo` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `publicaciones_ibfk_3` FOREIGN KEY (`idbarrio`) REFERENCES `barrios` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;

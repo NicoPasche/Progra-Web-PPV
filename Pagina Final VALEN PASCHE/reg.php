cc20a2929ae75ffff61f4d3df94ea8f2e30b1b52
Pablodubosc <pablodubosc@hotmail.com>
2019-07-05 18:43:32 -0300

    Update reg.php

--- Pagina Final VALEN PASCHE/reg.php
+++ Pagina Final VALEN PASCHE/reg.php
@@ -53,13 +53,36 @@
 
 
 
-<?php
-        $con=mysqli_connect("localhost","root","","users");
-        // Check connection
-        if (mysqli_connect_errno())
-              {
-              echo "Failed to connect to MySQL: " . mysqli_connect_error();
-              }
-      ?>
+
+		<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "nv_prop";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } 
+
+
+
+
+
+    $sql = "SELECT usuario FROM usuarios";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            echo "usuarios en uso: " . $row["usuario"] ."<br>";
+        }
+    } else {
+        echo "0 results";
+    }
+    $conn->close();
+?>
 </body>
 </html>
